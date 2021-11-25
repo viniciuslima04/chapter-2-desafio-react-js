93513b82b93ceb482576e4d8a60f20740baff78c
viniciuslima04 <vinicius.moura@timebrisa.com.br>
2021-11-25 09:50:27 -0300

    first commit

---
+++ src/hooks/useCart.tsx
@@ -0,0 +1,137 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+import { toast } from 'react-toastify';
+import { api } from '../services/api';
+import { Product, Stock } from '../types';
+
+interface CartProviderProps {
+  children: ReactNode;
+}
+
+interface UpdateProductAmount {
+  productId: number;
+  amount: number;
+}
+
+interface CartContextData {
+  cart: Product[];
+  addProduct: (productId: number) => Promise<void>;
+  removeProduct: (productId: number) => void;
+  updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
+}
+
+const CartContext = createContext<CartContextData>({} as CartContextData);
+
+export function CartProvider({ children }: CartProviderProps): JSX.Element {
+  const [cart, setCart] = useState<Product[]>(() => {
+    const storagedCart = localStorage.getItem('@RocketShoes:cart')
+
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
+
+    return [];
+  });
+
+  const addProduct = async (productId: number) => {
+    try {
+      const productAlreadyInCart = cart.find(product => product.id === productId)
+
+      if(!productAlreadyInCart) {
+        const { data: product } = await api.get<Product>(`products/${productId}`)
+        const { data: stock } = await api.get<Stock>(`stock/${productId}`)
+
+        if(stock.amount > 0) {
+          setCart([...cart, {...product, amount: 1}])
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify([...cart, {...product, amount: 1}]))
+          toast('Adicionado')
+          return;
+        }
+      }
+
+      if(productAlreadyInCart) {
+        const { data: stock } = await api.get(`stock/${productId}`)
+
+        if (stock.amount > productAlreadyInCart.amount) {
+          const updatedCart = cart.map(cartItem => cartItem.id === productId ? {
+            ...cartItem,
+            amount: Number(cartItem.amount) + 1
+          } : cartItem)
+  
+          setCart(updatedCart)
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+          return;
+        } else {
+          toast.error('Quantidade solicitada fora de estoque')
+        }
+      }
+    } catch {
+      toast.error('Erro na adição do produto')
+    }
+  };
+
+  const removeProduct = (productId: number) => {
+    try {
+      const productExists = cart.some(cartProduct => cartProduct.id === productId)
+      if(!productExists) {
+        toast.error('Erro na remoção do produto');
+        return
+      }
+
+      const updatedCart = cart.filter(cartItem => cartItem.id !== productId)
+      setCart(updatedCart)
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+    } catch {
+      toast.error('Erro na remoção do produto');
+    }
+  };
+
+  const updateProductAmount = async ({
+    productId,
+    amount,
+  }: UpdateProductAmount) => {
+    try {
+      if(amount < 1){
+        toast.error('Erro na alteração de quantidade do produto');
+        return
+      }
+
+      const response = await api.get(`/stock/${productId}`);
+      const productAmount = response.data.amount;
+      const stockIsFree = amount > productAmount
+
+      if(stockIsFree) {
+        toast.error('Quantidade solicitada fora de estoque')
+        return
+      }
+
+      const productExists = cart.some(cartProduct => cartProduct.id === productId)
+      if(!productExists) {
+        toast.error('Erro na alteração de quantidade do produto');
+        return
+      }
+
+      const updatedCart = cart.map(cartItem => cartItem.id === productId ? {
+        ...cartItem,
+        amount: amount
+      } : cartItem)
+      setCart(updatedCart)
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+    } catch {
+      toast.error('Erro na alteração de quantidade do produto');
+    }
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addProduct, removeProduct, updateProductAmount }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart(): CartContextData {
+  const context = useContext(CartContext);
+
+  return context;
+}
---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import {
+  MdDelete,
+  MdAddCircleOutline,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+
+import { useCart } from '../../hooks/useCart';
+import { formatPrice } from '../../util/format';
+import { Container, ProductTable, Total } from './styles';
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  amount: number;
+}
+
+const Cart = (): JSX.Element => {
+  const { cart, removeProduct, updateProductAmount } = useCart();
+
+  const cartFormatted = cart.map(product => ({
+    ...product, priceFormatted: formatPrice(product.price), subtotal: formatPrice(product.price * product.amount)
+  }))
+  const total =
+    formatPrice(
+      cartFormatted.reduce((sumTotal, product) => {
+        sumTotal += product.price * product.amount
+
+        return sumTotal
+      }, 0)
+    )
+
+  function handleProductIncrement(product: Product) {
+    const IncrementArguments = {
+      productId: product.id,
+      amount: product.amount + 1
+    }
+    updateProductAmount(IncrementArguments)
+  }
+
+  function handleProductDecrement(product: Product) {
+    const IncrementArguments = {
+      productId: product.id,
+      amount: product.amount - 1
+    }
+    updateProductAmount(IncrementArguments)
+  }
+
+  function handleRemoveProduct(productId: number) {
+    removeProduct(productId)
+  }
+
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th aria-label="product image" />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th aria-label="delete icon" />
+          </tr>
+        </thead>
+        <tbody>
+          {cartFormatted.map(product => (
+            <tr data-testid="product" key={product.id}>
+            <td>
+              <img src={product.image} alt={product.title} />
+            </td>
+            <td>
+              <strong>{product.title}</strong>
+              <span>{product.priceFormatted}</span>
+            </td>
+            <td>
+              <div>
+                <button
+                  type="button"
+                  data-testid="decrement-product"
+                disabled={product.amount <= 1}
+                onClick={() => handleProductDecrement(product)}
+                >
+                  <MdRemoveCircleOutline size={20} />
+                </button>
+                <input
+                  type="text"
+                  data-testid="product-amount"
+                  readOnly
+                  value={product.amount}
+                />
+                <button
+                  type="button"
+                  data-testid="increment-product"
+                onClick={() => handleProductIncrement(product)}
+                >
+                  <MdAddCircleOutline size={20} />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>{product.subtotal}</strong>
+            </td>
+            <td>
+              <button
+                type="button"
+                data-testid="remove-product"
+              onClick={() => handleRemoveProduct(product.id)}
+              >
+                <MdDelete size={20} />
+              </button>
+            </td>
+          </tr>
+          ))}
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>{total}</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+};
+
+export default Cart;
